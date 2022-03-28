7fa2d059bd7949a01f344c2e7e470ef5469bb176
Morgan Anders <MorganAndersDP@gmail.com>
2022-03-28 11:47:21 -0700

    Deleted Code

--- client/src/substring.js
+++ client/src/substring.js
@@ -1,15 +1,17 @@
-function addStr(str, index, stringToAdd){
-    return str.substring(0, index) + stringToAdd + str.substring(index, str.length);
-    }
-    let cursorPosition = str.length + positionValue;
-    // positionValue = 0;
-    // leftArrow then positionValue--   --> must be bigger or eq than 0
-    // rightArrow then positionValue++  --> must be less or eq than str.length
+// let str;
+// function addStr(str, index, stringToAdd) {
+//     return str.substring(0, index) + stringToAdd + str.substring(index, str.length);
+// }
 
-    let str = "This is a string";
-    let cursor = "|";
-    let stringToAdd = key;     //whatever key is being pressed;
-    
-    console.log(addStr(str, cursorPosition, stringToAdd)); //outPut : "This is a modified string"
+// // positionValue = 0;
+// // leftArrow then positionValue--   --> must be bigger or eq than 0
+// // rightArrow then positionValue++  --> must be less or eq than str.length
 
-export default addStr;
+// str = "This is a string";
+// // let cursor = "|";
+// // let stringToAdd = key;     //whatever key is being pressed;
+// // let cursorPosition = str.length + positionValue;
+// console.log(addStr(str, cursorPosition, stringToAdd)); //outPut : "This is a modified string"
+
+// export default addStr;
+
